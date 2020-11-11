5053f70776e5410e7c68be609817c4396a1cb459
Francesco Dente <dente.francesco1996@gmail.com>
2020-11-11 20:49:53 +0100

    Create TitleBar component

---
+++ tourna-pro-webapp/src/components/ui/TitleBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="title-bar">
+    {{appName}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleBar',
+  props: {
+    appName: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.title-bar {
+  background-color: $color-primary;
+  color: white;
+  padding: 5%;
+  font-size: 2rem;
+  font-weight: bold;
+}
+</style>
