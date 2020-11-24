478295ef605aa3172becfb37dae2739f35f7a977
Francesco Dente <dente.francesco1996@gmail.com>
2020-11-24 17:23:51 +0100

    Create teams page

---
+++ tourna-pro-webapp/src/components/teams/TeamCard.vue
@@ -0,0 +1,79 @@
+<template>
+  <a @click="$emit('click', $event)">
+    <div class="team-card">
+      <div class="image-container">
+        <img :src="team.image" :alt="`${team.name}'s team image`">
+      </div>
+      <div class="team-info">
+        <div class="info-bar">
+          <span class="team-name">{{ team.name }}</span>
+          <text-icon icon="fas fa-user-friends" :text="String(team.members)" />
+        </div>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+import TextIcon from '../ui/TextIcon.vue'
+export default {
+  components: { TextIcon },
+  props: {
+    team: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.team-card {
+  width: 100%;
+  margin-bottom: 20px;
+  height: 20vh;
+  position: relative;
+
+  .image-container {
+    height: 100%;
+    width: 100%;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  .team-info {
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    height: 100%;
+    position: absolute;
+    background: rgba(0, 0, 0, 0.3);
+    z-index: 10;
+
+    &:hover {
+      background: rgba(0, 0, 0, 0.5);
+      cursor: pointer;
+    }
+
+    .info-bar {
+      color: white;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: baseline;
+      position: absolute;
+      bottom: 0px;
+      width: 100%;
+      padding: 15px 20px;
+      font-size: 1.3rem;
+
+      .team-name {
+        font-weight: bold;
+      }
+    }
+  }
+}
+
+
+</style>
---
+++ tourna-pro-webapp/src/views/teams/TeamsHome.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Le mie squadre</h1>
+    <div class="teams-container">
+      <team-card :team="team" v-for="team in teams" :key="team.id" @click="teamClicked(team)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TeamCard from '../../components/teams/TeamCard.vue'
+export default {
+  components: { TeamCard },
+  data() {
+    return {
+      teams: [
+        {
+          id: 1,
+          name: 'Inazuma Eleven',
+          image: 'https://cdn.myanimelist.net/s/common/uploaded_files/1442407024-2d1f72c28647fc4a278cdd6fd0b14eb1.jpeg',
+          members: 11
+        },
+        {
+          id: 2,
+          name: 'Rin Tin Team',
+          image: 'https://cdn.myanimelist.net/s/common/uploaded_files/1442407024-2d1f72c28647fc4a278cdd6fd0b14eb1.jpeg',
+          members: 12
+        }
+      ]
+    }
+  },
+  methods: {
+    teamClicked() {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.teams-container {
+  padding: 10px;
+}
+</style>
