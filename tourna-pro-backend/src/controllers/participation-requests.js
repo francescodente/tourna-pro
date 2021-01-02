3f1c1053290055e0dc05638fe4de510383b48f60
Samuele Burattini <samuele.burattini@studio.unibo.it>
2021-01-02 22:30:10 +0100

    Start working on participation request logic controls

--- tourna-pro-backend/src/controllers/participation-requests.js
+++ tourna-pro-backend/src/controllers/participation-requests.js
@@ -1,5 +1,6 @@
-const { ParticipationRequests } = require('../models')
-const { ok, created, notFound } = require('../utils/action-results')
+const { ParticipationRequests, Tournament } = require('../models');
+const { isIndividual, isTeam } = require('../models/tournament-modes');
+const { ok, created, notFound, notAllowed, badRequest } = require('../utils/action-results')
 
 function participationRequestsDto(participationRequest, person) {
   return {
@@ -22,12 +23,47 @@ function requestNotFound(id, reqId) {
   return `Could not found request with id ${reqId} for tournament with id ${id}`
 }
 
+function notAllowedMessage(id, mode) {
+  return `Participant is not allowed for tournament ${id} that is for ${mode}`
+}
+
 exports.addParticipationRequest = async function (req) {
+  let tournament = await Tournament.findById(req.params.id)
+  if (!tournament) {
+    tournamentNotFound(req.params.id)
+  }
+  switch (req.body.type) {
+    case 'PERSON':
+      if(!req.body.person){
+        return badRequest("Person not defined")
+      }
+      if(!isIndividual(tournament.mode)){
+        return notAllowed(notAllowedMessage(req.params.id, tournament.mode))
+      }
+      break;
+    case 'USER':
+      if(!req.body.userId){
+        return badRequest("UserId not defined")
+      }
+      if(!isIndividual(tournament.mode)){
+        return notAllowed(notAllowedMessage(req.params.id, tournament.mode))
+      }
+      break;
+    case 'TEAM':
+      if(!req.body.teamId){
+        return badRequest("TeamId not defined")
+      }
+      if(!isTeam(tournament.mode)){
+        return notAllowed(notAllowedMessage(req.params.id, tournament.mode))
+      }
+      break;
+    default: return badRequest("Unsupported type")
+  }
   let participationRequestModel = new ParticipationRequest({
     type: req.body.type,
     person: {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName
+      firstName: req.body.person.firstName,
+      lastName: req.body.person.lastName
     },
     userId: req.body.userId,
     teamId: req.body.teamId,
@@ -46,7 +82,7 @@ exports.getAllParticipationRequests = async function (req) {
 }
 
 exports.removeParticipationRequest = async function (req) {
-  let deleteParticipationRequest = await ParticipationRequest.findeByIdAndRemove(req.params.requestId)
+  let deleteParticipationRequest = await ParticipationRequest.findByIdAndRemove(req.params.requestId)
   if (!deleteParticipationRequest) {
     return notFound(requestNotFound(req.params.id, req.params.requestId))
   }
--- tourna-pro-backend/src/models/tournament-modes.js
+++ tourna-pro-backend/src/models/tournament-modes.js
@@ -4,4 +4,12 @@ exports.modeExists = function(mode){
   return values.includes(mode)
 }
 
+exports.isIndividual = function(mode) {
+  return mode == values[1]
+}
+
+exports.isTeam = function(mode){
+  return mode == values[0]
+}
+
 exports.modes = values
