4781b9faf44a7c1eeb475900e23534a966a09d4f
Samuele Burattini <samuele.burattini@studio.unibo.it>
2021-01-02 20:16:14 +0100

    update team schema to check for consistency

--- tourna-pro-backend/src/models/team.js
+++ tourna-pro-backend/src/models/team.js
@@ -3,6 +3,6 @@ const { createSchema } = require('./model-utils')
 module.exports = createSchema('Team', 'Teams', mongoose => ({
   name: String,
   imageUrl: String,
-  members: [mongoose.ObjectId],
-  creatorId: mongoose.ObjectId
+  members: [{type: mongoose.ObjectId, ref:'User'}],
+  creatorId: [{type: mongoose.ObjectId, ref:'User'}]
 }))
