1862a812312da44b9ca0cf9a7bf19c60a1978a63
Samuele Burattini <samuele.burattini@studio.unibo.it>
2021-01-08 10:48:45 +0100

    Implemented user and authentication data access

--- tourna-pro-webapp/src/data-access/backend/authentication.js
+++ tourna-pro-webapp/src/data-access/backend/authentication.js
@@ -1,5 +1,10 @@
 import http from './http'
 
 export default {
-
+  login: (identifier, password) => {
+    http.post(`/login`, { identifier: identifier, password: password })
+  },
+  changePassword: (userId, oldPassword, newPassword) => {
+    http.put(`/users/${userId}/password`, { oldPassword: oldPassword, newPassword: newPassword })
+  }
 }
--- tourna-pro-webapp/src/data-access/backend/users.js
+++ tourna-pro-webapp/src/data-access/backend/users.js
@@ -1,5 +1,8 @@
 import http from './http'
 
 export default {
-
+  register: (user) => http.post(`/users`,user),
+  getUser: (id) => http.get(`/users/${id}`),
+  updateUser: (id) => http.put(`/users/${id}`),
+  updateImage: (id) => http.put(`/users/${id}/image`)
 }
