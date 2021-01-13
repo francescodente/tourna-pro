166c9340eec1fcc5144bafe3cbb0ac186ccbd39e
FRANCESCA TONETTI <francesca.tonetti@studio.unibo.it>
2021-01-13 12:33:13 +0100

    Add backend endpoint tournaments-type and tournaments-owners.

--- tourna-pro-webapp/src/data-access/backend/tournament-owners.js
+++ tourna-pro-webapp/src/data-access/backend/tournament-owners.js
@@ -1,5 +1,7 @@
 import http from './http'
 
 export default {
-  
+  getAll: id => http.get(`/tournaments/${id}/owners`),
+  addOwner: (id, owner) => http.post(`/tournaments/${id}/owners`, owner),
+  delete: (id, userId) => http.delete(`/tournaments/${id}/owners/${userId}`)
 }
--- tourna-pro-webapp/src/data-access/backend/tournament-types.js
+++ tourna-pro-webapp/src/data-access/backend/tournament-types.js
@@ -1,4 +1,5 @@
 import http from './http'
 
 export default {
+  getAll: filters => http.get(`/tournamenttypes`, filters)
 }
