ed72ec80360d85ce211f10d3a107a260d7f44d23
FRANCESCA TONETTI <francesca.tonetti@studio.unibo.it>
2021-01-13 12:29:27 +0100

    Add backend endpoint teams and team-members.

--- tourna-pro-webapp/src/data-access/backend/team-members.js
+++ tourna-pro-webapp/src/data-access/backend/team-members.js
@@ -1,4 +1,7 @@
 import http from './http'
 
 export default {
+  get: (id) => http.get(`/teams/${id}/members`),
+  addMember: (id, member) => http.post(`/teams/${id}/members`, member),
+  delete: (id, userId) => http.delete(`/teams/${id}/members/${userId}`)
 }
--- tourna-pro-webapp/src/data-access/backend/teams.js
+++ tourna-pro-webapp/src/data-access/backend/teams.js
@@ -1,5 +1,9 @@
 import http from './http'
 
 export default {
-
+  create: team => http.post(`/teams`, team),
+  getAll: filters => http.get(`/teams`, filters),
+  update: (id, team) => http.put(`/teams/${id}`, team),
+  delete: id => http.delete(`teams/${id}`),
+  updateImage: id => http.put(`/teams/${id}/image`)
 }
