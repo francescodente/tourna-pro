bf844b25114e0bc603597d068da3d098e84d3fc7
FRANCESCA TONETTI <francesca.tonetti@studio.unibo.it>
2021-01-13 12:36:54 +0100

    Add backend endpoint matches.

--- tourna-pro-webapp/src/data-access/backend/matches.js
+++ tourna-pro-webapp/src/data-access/backend/matches.js
@@ -1,5 +1,7 @@
 import http from './http'
 
 export default {
-
+  getAll: id => http.get(`/tournaments/${id}/matches`),
+  update: (id, matchId, matchResult) => http.put(`/tournaments/${id}/matches/${matchId}`, matchResult),
+  lockMatches: (id, matches) => http.put(`/tournaments/${id}/matches`, matches)
 }
