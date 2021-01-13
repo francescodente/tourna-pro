c779732d130550887d916d5d55ce56108985ab76
FRANCESCA TONETTI <francesca.tonetti@studio.unibo.it>
2021-01-13 12:20:36 +0100

    Add backend endpoint participants and participants-request.

--- tourna-pro-webapp/src/data-access/backend/participants.js
+++ tourna-pro-webapp/src/data-access/backend/participants.js
@@ -1,5 +1,6 @@
 import http from './http'
 
 export default {
-
+  getAll: (id) => http.get(`/tournaments/${id}/participants`),
+  delete: (id, participantId) => http.delete(`/tournaments/${id}/participant/${participantId}`)
 }
