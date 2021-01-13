b087b373d2b36d4b8af1c161765fc06218921810
FRANCESCA TONETTI <francesca.tonetti@studio.unibo.it>
2021-01-13 11:33:56 +0100

    Update backend for user and interests adding body into update operation.

--- tourna-pro-webapp/src/data-access/backend/interests.js
+++ tourna-pro-webapp/src/data-access/backend/interests.js
@@ -2,5 +2,5 @@ import http from './http'
 
 export default {
   getAll: (id) => http.get(`/users/${id}/interests`),
-  update: (id, interest) => http.put(`users/${id}/interests`, interest)
+  update: (id, interests) => http.put(`users/${id}/interests`, interests)
 }
--- tourna-pro-webapp/src/data-access/backend/users.js
+++ tourna-pro-webapp/src/data-access/backend/users.js
@@ -3,6 +3,6 @@ import http from './http'
 export default {
   register: (user) => http.post(`/users`,user),
   getUser: (id) => http.get(`/users/${id}`),
-  updateUser: (id) => http.put(`/users/${id}`),
+  updateUser: (id, user) => http.put(`/users/${id}`, user),
   updateImage: (id) => http.put(`/users/${id}/image`)
 }
