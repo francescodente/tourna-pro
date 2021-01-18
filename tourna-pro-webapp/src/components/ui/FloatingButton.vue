5d1b0142a09c80b89b97b8894eb03bedfa434d45
Francesco Dente <dente.francesco1996@gmail.com>
2021-01-18 17:52:32 +0100

    Add space under floating button

--- tourna-pro-webapp/src/components/ui/FloatingButton.vue
+++ tourna-pro-webapp/src/components/ui/FloatingButton.vue
@@ -1,7 +1,10 @@
 <template>
-  <button @click="$emit('click', $event)">
-    <slot><i :class="icon" /></slot>
-  </button>  
+  <div class="floating-button-container">
+    <div class="floating-button-spacing"></div>
+    <button @click="$emit('click', $event)">
+      <slot><i :class="icon" /></slot>
+    </button>
+  </div>  
 </template>
 
 <script>
@@ -16,18 +19,27 @@ export default {
 $distance: 25px;
 $size: 70px;
 
-button {
-  position: fixed;
-  right: $distance;
-  bottom: $distance + $bottom-navbar-height;
-  height: $size;
-  width: $size;
-  border: none;
-  border-radius: 100%;
-  background-color: $color-complementary;
-  color: white;
-  font-size: 1.5rem;
-  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
-  border:4px solid $color-complementary-light;
+.floating-button-container {
+  width: 100%;
+
+  .floating-button-spacing {
+    padding-top: $distance + $size;
+  }
+
+  button {
+    position: fixed;
+    right: $distance;
+    bottom: $distance + $bottom-navbar-height;
+    height: $size;
+    width: $size;
+    border: none;
+    border-radius: 100%;
+    background-color: $color-complementary;
+    color: white;
+    font-size: 1.5rem;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+    border:4px solid $color-complementary-light;
+    z-index: 10;
+  }
 }
 </style>
