b07c9f7bd809f8457c8a07351c61f606840426df
Samuele Burattini <samuele.burattini@studio.unibo.it>
2021-01-22 11:40:35 +0100

    fix modify profile

--- tourna-pro-webapp/src/views/profile/UserModifyProfile.vue
+++ tourna-pro-webapp/src/views/profile/UserModifyProfile.vue
@@ -1,54 +1,67 @@
 <template>
-  <div v-if="user">
-    <avatar size="5em" :src="user.imageUrl" />
-    <div class="nickname">@{{ user.username }}</div>
+  <div>
+    <b-alert v-model="showAlert" variant="warning" dismissible fade>
+      Le tue informazioni sono state aggiornate
+    </b-alert>
+    <div v-if="user" class="main">
+      <avatar size="5em" :src="user.imageUrl" />
+      <div class="nickname">@{{ user.username }}</div>
 
-    <div class="inputs">
-      <simple-form submitMessage="Salva Modifiche">
-        <user-info :value="user"/>
-      </simple-form>
+      <div class="inputs">
+        <simple-form submitMessage="Salva Modifiche" @submit="updateUser">
+          <user-info v-model="user" />
+        </simple-form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import dataAccess from '@/data-access'
+import dataAccess from "@/data-access";
 import Avatar from "../../components/profile/Avatar.vue";
 import UserInfo from "../../components/registration/UserInfo.vue";
 import SimpleInput from "../../components/ui/SimpleInput.vue";
-import SimpleForm from '../../components/ui/SimpleForm.vue';
+import SimpleForm from "../../components/ui/SimpleForm.vue";
 
 export default {
   components: { Avatar, UserInfo, SimpleInput, SimpleForm },
   data: function () {
     return {
-      user: null
+      user: null,
+      showAlert: false,
     };
   },
   methods: {
     async fetchUser() {
-      this.user = await dataAccess.users.getUser(this.userId)
-    }
+      this.user = await dataAccess.users.getUser(this.userId);
+      this.user.birthDate = this.user.birthDate.split("T")[0];
+    },
+    async updateUser() {
+      this.user = await dataAccess.users.updateUser(this.userId, this.user);
+      this.showAlert = true;
+    },
   },
   computed: {
     userId() {
-      return this.$route.params.id
-    }
+      return this.$route.params.id;
+    },
   },
   async created() {
-    await this.fetchUser()
+    await this.fetchUser();
   },
 };
 </script>
 
 <style lang="scss" scoped>
-
-.nickname{
+.main {
+  padding: 20px 30px;
+}
+.nickname {
   font-size: 10pt;
   text-align: center;
 }
 
-.inputs{
+.inputs {
   margin-top: 5%;
   height: 100%;
   width: 90%;
