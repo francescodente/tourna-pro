eda5fbb5c95e0a15f63bef0b18fff89c2bdc7a38
Samuele Burattini <samuele.burattini@studio.unibo.it>
2021-01-24 17:45:46 +0100

    fixes on getAll tournaments

--- tourna-pro-backend/src/routes/participation-requests.js
+++ tourna-pro-backend/src/routes/participation-requests.js
@@ -3,8 +3,10 @@ const { mapControllerRoutes, action } = require('./route-utils')
 
 module.exports = mapControllerRoutes('participation-requests', function (app, controller) {
     app.route('/requests')
-        .post(authorize, action(controller.addParticipationRequest))
         .get(authorize, action(controller.getAllParticipationRequests))
+
+    app.route('/tournaments/:id/requests')
+        .post(authorize, action(controller.addParticipationRequest))
 
     app.route('/tournaments/:id/requests/:requestId')
         .delete(authorize, action(controller.removeParticipationRequest))
