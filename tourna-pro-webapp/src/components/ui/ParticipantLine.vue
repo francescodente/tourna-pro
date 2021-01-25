334d19c20693500bd42cfdfb7975a72f1041c6fa
Francesco Dente <dente.francesco1996@gmail.com>
2021-01-25 19:22:11 +0100

    Extract user line features to prepare for team participant line

---
+++ tourna-pro-webapp/src/components/ui/ParticipantLine.vue
@@ -0,0 +1,68 @@
+<template>
+  <list-item class="participant-line" :canSelect="canSelect" @selected="$emit('selected')">
+    <avatar :src="imageUrl"/>
+    <div class="participant-name">
+      {{ name }}
+      <span v-if="secondaryName" class="real-name">({{ secondaryName }})</span>
+      <i v-if="isOwner" class="fas fa-crown owner"></i>
+    </div>
+    <a v-if="canDelete" @click="$emit('deleted')" class="delete-button">
+      <i class="fas fa-times"></i>
+    </a>
+  </list-item>
+</template>
+
+<script>
+import Avatar from '../profile/Avatar.vue'
+import ListItem from '../ui/ListItem.vue'
+export default {
+  components: { Avatar, ListItem },
+  props: {
+    name: String,
+    secondaryName: String,
+    imageUrl: String,
+    isOwner: {
+      type: Boolean,
+      default: false
+    },
+    canDelete: {
+      type: Boolean,
+      default: true
+    },
+    canSelect: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.participant-line {
+  font-weight: bold;
+  color:black;
+
+  .participant-name {
+    flex: 1;
+    text-align: left;
+    margin-left: 10px;
+
+    .real-name {
+      color: gray;
+    }
+
+    .owner {
+      color: $color-secondary1;
+      margin-left: 15px;
+    }
+  }
+
+  .delete-button {
+    color: $color-secondary2;
+    font-size: 1.3rem;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+</style>
--- tourna-pro-webapp/src/components/users/UserLine.vue
+++ tourna-pro-webapp/src/components/users/UserLine.vue
@@ -1,22 +1,22 @@
 <template>
-  <list-item class="user-line" :canSelect="canSelect" @selected="$emit('selected', user)">
-    <avatar :src="user.imageUrl"/>
-    <div class="user-username">
-      {{ user.username }}
-      <span class="real-name">({{ user.firstName }} {{ user.lastName }})</span>
-      <i v-if="isOwner" class="fas fa-crown owner"></i>
-    </div>
-    <a v-if="canDelete && user.id != $store.getters.userId" @click="$emit('deleted', $event)" class="delete-button">
-      <i class="fas fa-times"></i>
-    </a>
-  </list-item>
+  <participant-line
+    :name="user.username"
+    :secondaryName="`${user.firstName} ${user.lastName}`"
+    :imageUrl="user.imageUrl"
+    :canDelete="canDelete && user.id != $store.getters.userId"
+    :canSelect="canSelect"
+    :isOwner="isOwner"
+    @selected="$emit('selected', user)"
+    @deleted="$emit('deleted')"
+  />
 </template>
 
 <script>
 import Avatar from '../profile/Avatar.vue'
 import ListItem from '../ui/ListItem.vue'
+import ParticipantLine from '../ui/ParticipantLine.vue'
 export default {
-  components: { Avatar, ListItem },
+  components: { Avatar, ListItem, ParticipantLine },
   props: {
     user: Object,
     isOwner: Boolean,
@@ -31,33 +31,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.user-line {
-  font-weight: bold;
-  color:black;
-
-  .user-username {
-    flex: 1;
-    text-align: left;
-    margin-left: 10px;
-
-    .real-name {
-      color: gray;
-    }
-
-    .owner {
-      color: $color-secondary1;
-      margin-left: 15px;
-    }
-  }
-
-  .delete-button {
-    color: $color-secondary2;
-    font-size: 1.3rem;
-    &:hover {
-      cursor: pointer;
-    }
-  }
-}
-</style>
