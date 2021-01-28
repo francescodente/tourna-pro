7d470603d0dc732ccb99866be0aee19fa2ac8e04
Samuele Burattini <samuele.burattini@studio.unibo.it>
2021-01-28 15:44:05 +0100

    update on achievements

--- tourna-pro-backend/src/models/achievements.js
+++ tourna-pro-backend/src/models/achievements.js
@@ -6,6 +6,12 @@ const values = [
     image: ""
   },
   {
+    id: "GIOCO_DI_SQUADRA",
+    name: "Gioco di squadra",
+    description: "Partecipa ad un torneo in modalità squadre.",
+    image: ""
+  },
+  {
     id: "AND_THE_WINNER_IS",
     name: "And the winner is...",
     description: "Vinci il tuo primo torneo!",
@@ -17,6 +23,79 @@ const values = [
     description: "Crea una squadra",
     image: ""
   },
+  {
+    id: "BUONI_AMICI",
+    name: "Buoni amici",
+    description: "Entra in una squadra esistente",
+    image: ""
+  },
+  {
+    id: "MAESTRO_DEI_GIOCHI",
+    name: "Maestro dei giochi",
+    description: "Crea il tuo primo torneo",
+    image: ""
+  },
+  {
+    id: "PREMIO_DI_CONSOLAZIONE",
+    name: "Premio di consolazione",
+    description: "Arriva ultimo ad un torneo",
+    image: ""
+  },
+  {
+    id: "COLLEZIONISTA",
+    name: "Collezionista",
+    description: "vinci 3 tornei",
+    image: ""
+  },
+  {
+    id: "GOLEADOR",
+    name: "Goleador",
+    description: "Vinci un torneo di Calcio",
+    image: ""
+  },
+  {
+    id: "GRAN_MAESTRO_DEGLI_SCACCHI",
+    name: "Gran Maestro degli Scacchi",
+    description: "Vinci un torneo di Scacchi",
+    image: ""
+  },
+  {
+    id: "PUZZLE_DEL_MILLENNIO",
+    name: "Puzzle del Millennio",
+    description: "Vinci un torneo di Yu-Gi-Oh!",
+    image: ""
+  },
+  {
+    id: "PUZZLE_DEL_MILLENNIO",
+    name: "Puzzle del Millennio",
+    description: "Vinci un torneo di Yu-Gi-Oh!",
+    image: ""
+  },
+  {
+    id: "CECCHINO_UBRIACONE",
+    name: "Cecchino ubriacone",
+    description: "Vinci un torneo di Beer-Pong",
+    image: ""
+  },
+  {
+    id: "RE_DELLA_SPIAGGIA",
+    name: "Re della spiaggia",
+    description: "Vinci un torneo di Beach Volley",
+    image: ""
+  },
+  {
+    id: "THE_JOCKER",
+    name: "The Jocker",
+    description: "Vinci un torneo di Burraco",
+    image: ""
+  },
+  {
+    id: "SEI_UN_ASSO",
+    name: "Sei un Asso",
+    description: "Vinci un torneo di Briscola",
+    image: ""
+  },
+  
 
 ]
 
