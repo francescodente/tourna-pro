8e0c2a710c17340bbdef6a1d53ab66205106de09
Samuele Burattini <samuele.burattini@studio.unibo.it>
2021-01-29 19:33:36 +0100

    Add font

--- tourna-pro-webapp/src/App.vue
+++ tourna-pro-webapp/src/App.vue
@@ -57,12 +57,14 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 
   .main-container {
     padding-top: $titlebar-height;
